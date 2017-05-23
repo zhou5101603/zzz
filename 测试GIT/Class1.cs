8bef5aa0d94401eb8f33a13e5e751d300e55564e
zhou5101603 <as5101603@qq.com>
2017-05-23 19:01:07 +0800

    11222233

---
+++ 测试GIT/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 测试GIT
+{
+    class Class1
+    {
+        string Name;
+      public string Name1
+        {
+            get
+            {
+                return Name;
+            }
+
+            set
+            {
+                Name = value;
+            }
+        }
+    }
+}
